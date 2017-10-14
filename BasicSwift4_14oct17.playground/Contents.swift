5ecfbd1f52c86d095f5b6872f438da9dab92f50e
MasterUNG <phrombutr@gmail.com>
2017-10-14 10:55:31 +0700

    Finish

--- BasicSwift4_14oct17.playground/Contents.swift
+++ BasicSwift4_14oct17.playground/Contents.swift
@@ -12,6 +12,24 @@ var statusBool = true
 nameSting = "Doramy"
 numberInt = 222
 
+//การประกาศตัวแปรที่ไม่สามารถแก้ไขค่า คือ ค่าคงที่นั้นเอง
+let surnameString = "MySurname"
+//surnameString = "ChangeSur"
+
+//การกำหนด Data Type
+var studentString: String = "Nopita"
+var num1: Int = 222
+
+//Display on Teminal
+print("Display on Termial แสดงบน Terminal")
+print("Name ==> \(nameSting)")
+print("number ==> \(numberInt + 100)")
+
+
+
+
+
+
 
 
 
