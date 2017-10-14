236e04c0c18893ea68c4ce5b8997d48afd4a1a7c
MasterUNG <phrombutr@gmail.com>
2017-10-14 15:47:19 +0700

    Start Dictionary

---
+++ DictionaryVarialble.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Dictinary Format
+var myDictionaryString = ["key1":"Value1", "key2":"Value2", "key3":"Value3"]
+
+//การใช้ งาน Dictionary
+print("myDictioaryString ที่มี key มีค่าเท่ากับ key3 คือ \(String(describing: myDictionaryString["key3"]))")
+
+//การเพิ่ม สมาชิคให้ตัวแปร ประเภท Dictionary
+myDictionaryString["key4"] = "Doramon"
+
+print("key ==> key4 Value ==> \(String(describing: myDictionaryString["key4"]))")
+
