bc18e1f4a232728d4b9999cc9a06a9b5083fd0e0
MasterUNG <phrombutr@gmail.com>
2017-10-14 10:39:14 +0700

    Variable type var

--- BasicSwift4_14oct17.playground/Contents.swift
+++ BasicSwift4_14oct17.playground/Contents.swift
@@ -2,4 +2,16 @@
 
 import UIKit
 
-var str = "Hello, Master"
+//Variable and Constant การประกาศตัวแปร และ ค่าคงที่
+//การประกาศตัวแปรแบบ แก้ไขค่าได้
+var nameSting = "Doramon"
+var numberInt = 123
+var numberDoubel = 12.345
+var statusBool = true
+
+nameSting = "Doramy"
+numberInt = 222
+
+
+
+
