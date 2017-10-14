f31d1c89b63cf33965263df76eab8f248bad0788
MasterUNG <phrombutr@gmail.com>
2017-10-14 14:14:19 +0700

    Success

--- LearnArray.playground/Contents.swift
+++ LearnArray.playground/Contents.swift
@@ -2,4 +2,26 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var nameArrayString = ["AAA", "BBB", "CCC", "DDD"]
+print("nameArray index at 2 ==> \(nameArrayString[2])")
+
+//ให้แสดงผลของ nameArrayString[0] + " " + nameArrayString[3]
+
+let mySpace = " "
+print("Answer ==> \(nameArrayString[0] + mySpace + nameArrayString[3])")
+
+//การหาขนาดของ Array
+let lengthArray = nameArrayString.count
+print("ขนาดของ Array มีค่า = \(lengthArray)")
+
+//การเพิ่ม สมาชิคให้ Array
+nameArrayString.append("Doramon")
+
+//การลบ สมาชิค
+nameArrayString
+nameArrayString.remove(at: 1)
+nameArrayString
+
+
+
+
