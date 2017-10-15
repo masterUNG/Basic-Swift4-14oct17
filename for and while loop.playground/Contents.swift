aaa69257790deeec584e9efcaed2373bca17313d
MasterUNG <phrombutr@gmail.com>
2017-10-15 11:53:24 +0700

    while Loop OK

--- for and while loop.playground/Contents.swift
+++ for and while loop.playground/Contents.swift
@@ -2,15 +2,39 @@
 
 import UIKit
 
+
 //Sample How to user For Loop
 let nameString = ["Doramon", "Nopita", "Suneko", "MasterUNG"]
+
+
+
+
 var timesInt = 0
 
+//Tyoe Sting
+//for strFriend in nameString {
+//    print("Name of Friend ==> \(strFriend)")
+//    timesInt += 1
+//    print("จำนวนรอบที่ \(timesInt)")
+//}
 
-for strFriend in nameString {
-    print("Name of Friend ==> \(strFriend)")
-    timesInt += 1
-    print("จำนวนรอบที่ \(timesInt)")
+//Type Integer
+//let numbrInt = [1,2,3,4,5]
+//for positionArray in numbrInt {
+//    print("Array[\(positionArray)] ==> \(numbrInt[positionArray-1])")
+//}
+
+//ตัวอย่าง การทำ While Loop
+var factorInt = 1   // lastest factorInt ==> 12
+while (factorInt <= 12) {
+    print("รอบที่ \(factorInt)")
+    factorInt += 1
 }
+
+
+
+
+
+
 
 
